ae576b2a58bf1686c168443de8eb4b61d2a2b393
Haku <hakudevtw@gmail.com>
2024-02-13 12:03:43 +0800

    feat: add base configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,30 +1,39 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'Docs with Tailwind',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-			customCss: ['./src/tailwind.css'],
-		}),
-		tailwind({ applyBaseStyles: false }),
-	],
+  // site: 'https://stargazers.club',
+  integrations: [
+    starlight({
+      title: `Haku の ノット`,
+      logo: {
+        light: "./src/assets/logo.webp",
+        dark: "./src/assets/logo.webp",
+        alt: "Haku Bear",
+      },
+      social: {
+        github: "https://github.com/withastro/starlight",
+      },
+      editLink: {
+        baseUrl: "https://github.com/withastro/starlight/edit/main/docs/",
+      },
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", link: "/guides/example/" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+      customCss: ["./src/tailwind.css"],
+    }),
+    tailwind({ applyBaseStyles: false }),
+  ],
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,18 +1,27 @@
-import colors from 'tailwindcss/colors';
-import starlightPlugin from '@astrojs/starlight-tailwind';
+import colors from "tailwindcss/colors";
+import starlightPlugin from "@astrojs/starlight-tailwind";
+
+// Generated color palettes
+// https://starlight.astro.build/guides/css-and-tailwind/#color-theme-editor
+const accent = { 200: "#c5c6e0", 600: "#6462a8", 900: "#2f2e4d", 950: "#222135" };
+const gray = {
+  100: "#f5f6f9",
+  200: "#ebeef4",
+  300: "#bfc2c9",
+  400: "#858c99",
+  500: "#525864",
+  700: "#333844",
+  800: "#222732",
+  900: "#16181d",
+};
 
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				// Your preferred accent color. Indigo is closest to Starlight’s defaults.
-				accent: colors.indigo,
-				// Your preferred gray scale. Zinc is closest to Starlight’s defaults.
-				gray: colors.zinc,
-			},
-		},
-	},
-	plugins: [starlightPlugin()],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: { accent, gray },
+    },
+  },
+  plugins: [starlightPlugin()],
 };
